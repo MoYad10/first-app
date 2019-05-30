90aee610e7eb5217748030c4be76c7efe6b4ff1f
MoYad10 <mevazi@gmail.com>
2019-05-29 23:28:55 -0400

    SPA git push -u origin master git status git commit -m SPA

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -7,8 +7,10 @@ import { AboutComponent } from '../about/about.component';
 import { ContactComponent } from '../contact/contact.component';
 
 export const routes: Routes = [
-  {path:'home', component: HomeComponent},
+  {path: 'home', component: HomeComponent},
   {path: 'menu', component: MenuComponent},
-  {path:'', redirectTo:'/home',pathMatch: 'full'}
+  {path: 'dishdetail/:id', component: DishdetailComponent},
+  {path: 'contactus',component: ContactComponent},
+  {path: '', redirectTo:'/home',pathMatch: 'full'}
 
 ]
